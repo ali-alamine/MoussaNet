67dc81b513ee179bfcec31994e8b1e082ba48aec
hassanhawi <hassanhawi8@gmail.com>
2018-09-13 13:29:22 +0300

    solve selection error

--- src/app/sell/sell.component.ts
+++ src/app/sell/sell.component.ts
@@ -12,22 +12,22 @@ import { ClientsService } from '../clients/clients.service';
   styleUrls: ['./sell.component.css']
 })
 export class SellComponent implements OnInit {
-  show=false;
+  show = false;
   panelOpenState = false;
-  itemForSell$ : any;
-  items : any;
-  clients : any;
-  rechargeCard:any;
-  offers:any;
-  credits:any;
-  showCredits:any;
-  accessories:any;
+  itemForSell$: any;
+  items: any;
+  clients: any;
+  rechargeCard: any;
+  offers: any;
+  credits: any;
+  showCredits: any;
+  accessories: any;
   private sub;
   public fullCardForm;
   public offersForm;
   public creditTransfersForm;
   public centralForm;
-  
+
   accessoriesForm: FormGroup;
   options;
   newItemForm: FormGroup;
@@ -42,62 +42,66 @@ export class SellComponent implements OnInit {
     private fb: FormBuilder,
     private formBuilder: FormBuilder,
     private router: Router,
-    private clientsService:ClientsService ){ }
+    private clientsService: ClientsService) { }
   ngOnInit() {
+
+    this.getRechargeCard();
+    this.getOffers();
+    this.getCreditsTransfers();
 
     this.fullCardForm = this.fb.group({
       debit: false,
       clientID: '',
-      itemID:'',
-      searchBarCode:'',
+      itemID: '',
+      searchBarCode: '',
       cardName: ['', Validators.required],
-      quantity:['', Validators.required],
+      quantity: ['', Validators.required],
       price: ['', Validators.required],
-      profit:''
+      profit: ''
     });
     this.offersForm = this.fb.group({
-      debit:false,
-      clientID:'',
-      itemID:'',
-      searchBarCode:'',
+      debit: false,
+      clientID: '',
+      itemID: '',
+      searchBarCode: '',
       company: ['', Validators.required],
       mounth: ['', Validators.required],
       credits: ['', Validators.required],
       price: ['', Validators.required]
     })
     this.creditTransfersForm = this.fb.group({
-      debit:false,
-      clientID:'',
-      company:['', Validators.required],
-      credits:['', Validators.required],
+      debit: false,
+      clientID: '',
+      company: ['', Validators.required],
+      credits: ['', Validators.required],
       price: ['', Validators.required]
     })
     this.accessoriesForm = this.fb.group({
-      clientID:'',
+      clientID: '',
       searchAccessories: '',
       items: this.fb.array([])
     });
     this.centralForm = this.fb.group({
       country: ['', Validators.required],
       mins: ['', Validators.required],
-      price: ['',Validators.required]
+      price: ['', Validators.required]
     });
     this.clientDebitForm = this.fb.group({
       clientID: [''],
       searchClient: '',
       clientName: ['']
     });
-    this.countrys=this.sellService.getCountry();
-    this.onAccessoriesNameChange();
+    this.countrys = this.sellService.getCountry();
+    // this.onAccessoriesNameChange();
     // this.addItem();
+
     this.onClientNameChange();
-    this.onRechargeCardChange();
+    // this.onRechargeCardChange();
     this.onOffersChange();
     this.onCreditsTransfersChange();
     this.onItemNameChange();
-    this.getRechargeCard();
-    this.getOffers();
-    this.getCreditsTransfers();
+
+
     // this.getAccessories();
 
   }
@@ -114,17 +118,17 @@ export class SellComponent implements OnInit {
       })
     });
   }
-  onAccessoriesQuantity(){
+  onAccessoriesQuantity() {
     this.fullCardForm.get('searchBarCode').valueChanges.subscribe(val => {
       var data = this.fullCardForm.get('searchBarCode').value;
-      for(var i=0;i<this.rechargeCard.length;i++){
-        if(this.rechargeCard[i].bar_code==data){
+      for (var i = 0; i < this.rechargeCard.length; i++) {
+        if (this.rechargeCard[i].bar_code == data) {
           this.fullCardForm.get('searchBarCode').setValue('');
           this.fullCardForm.get('itemID').setValue(this.rechargeCard[i].IID);
           this.fullCardForm.get('cardName').setValue(this.rechargeCard[i].name);
           this.fullCardForm.get('quantity').setValue(1);
           this.fullCardForm.get('price').setValue(this.rechargeCard[i].price);
-          var profit= this.rechargeCard[i].price-this.rechargeCard[i].cost;
+          var profit = this.rechargeCard[i].price - this.rechargeCard[i].cost;
           this.fullCardForm.get('profit').setValue(profit);
         }
       }
@@ -143,8 +147,8 @@ export class SellComponent implements OnInit {
   onOffersChange(): void {
     this.offersForm.get('searchBarCode').valueChanges.subscribe(val => {
       var data = this.offersForm.get('searchBarCode').value;
-      for(var i=0;i<this.offers.length;i++){
-        if(this.offers[i].bar_code==data){
+      for (var i = 0; i < this.offers.length; i++) {
+        if (this.offers[i].bar_code == data) {
           this.offersForm.get('searchBarCode').setValue('');
           this.offersForm.get('itemID').setValue(this.offers[i].IID);
           this.offersForm.get('company').setValue(this.offers[i].company);
@@ -163,28 +167,28 @@ export class SellComponent implements OnInit {
       this.changePriceCreditsTransfers();
     });
   }
-  changePriceCreditsTransfers(){
+  changePriceCreditsTransfers() {
     var company = this.creditTransfersForm.get('company').value;
     var credits = this.creditTransfersForm.get('credits').value;
     this.creditTransfersForm.get('price').setValue('');
-    for(var i=0;i<this.credits.length;i++){
-      if(this.credits[i].company==company && this.credits[i].num_of_credit==credits){
+    for (var i = 0; i < this.credits.length; i++) {
+      if (this.credits[i].company == company && this.credits[i].num_of_credit == credits) {
         this.creditTransfersForm.get('price').setValue(this.credits[i].price);
       }
     }
-    
+
   }
-  getRechargeCard(){
-    this.sellService.getRechargeCard().subscribe(Response=>{
+  getRechargeCard() {
+    this.sellService.getRechargeCard().subscribe(Response => {
       console.log(Response)
-        this.rechargeCard=Response;
-      })
+      this.rechargeCard = Response;
+    })
   }
-  getOffers(){
-    this.sellService.getOffers().subscribe(Response=>{
+  getOffers() {
+    this.sellService.getOffers().subscribe(Response => {
       console.log(Response)
-        this.offers=Response;
-      })
+      this.offers = Response;
+    })
   }
   // getAccessories(){
   //   this.sellService.getAccessories().subscribe(Response=>{
@@ -192,11 +196,11 @@ export class SellComponent implements OnInit {
   //       this.accessories=Response;
   //     })
   // }
-  getCreditsTransfers(){
-    this.sellService.getCreditsTransfers().subscribe(Response=>{
+  getCreditsTransfers() {
+    this.sellService.getCreditsTransfers().subscribe(Response => {
       console.log(Response)
-        this.credits=Response;
-      })
+      this.credits = Response;
+    })
   }
   onAccessoriesNameChange(): void {
     // this.supplyForm.get('searchSupplier').valueChanges.subscribe(val => {
@@ -210,32 +214,35 @@ export class SellComponent implements OnInit {
     //   })
     // });
   }
-  onClientNameChange():void{
+  onClientNameChange(): void {
     this.clientDebitForm.get('searchClient').valueChanges.subscribe(val => {
       var data = this.clientDebitForm.get('searchClient').value;
-      if(data=="")
-      {
-        this.clients=[];
+      if (data == "") {
+        this.clients = [];
         return;
       }
-    this.sellService.searchClient(data).subscribe(Response=>{
-        this.clients=Response;
+      this.sellService.searchClient(data).subscribe(Response => {
+        this.clients = Response;
       })
     });
   }
-  searchClientChange(id,name,phone){
+  searchClientChange(id, name, phone) {
     this.clientDebitForm.get('searchClient').setValue('');
     this.clientDebitForm.get('clientName').setValue(name);
     this.clientDebitForm.get('clientID').setValue(id);
     // console.log(this.clientDebitForm.value)
   }
-  selectRechargeCard(id,price,cost){
-    this.fullCardForm.get('searchBarCode').setValue('');
-    this.fullCardForm.get('itemID').setValue(id);
-    this.fullCardForm.get('quantity').setValue(1);
-    this.fullCardForm.get('price').setValue(price);
-    var profit= price-cost;
-    this.fullCardForm.get('profit').setValue(profit);
+  selectRechargeCard(event, rc) {
+    if (event.source.selected) {
+      this.fullCardForm.get('searchBarCode').setValue('');
+      this.fullCardForm.get('itemID').setValue(rc.id);
+      this.fullCardForm.get('quantity').setValue(1);
+      this.fullCardForm.get('price').setValue(rc.price);
+      var profit = rc.price - rc.cost;
+      this.fullCardForm.get('profit').setValue(profit);
+    }
+
+
 
   }
   addItem(name) {
@@ -252,14 +259,14 @@ export class SellComponent implements OnInit {
     this.itemsForm.removeAt(i);
     // this.onChanges();
   }
-  test(id, name,price,cost) {
+  test(id, name, price, cost) {
     this.accessoriesForm.get('searchAccessories').setValue('');
-    var profit=price-cost;
+    var profit = price - cost;
     const item = this.fb.group({
       name: [name],
       itemID: [id],
       quantity: [1, Validators.required],
-      profit:profit,
+      profit: profit,
       price: [price, Validators.required],
     });
     this.itemsForm.push(item);
@@ -270,7 +277,7 @@ export class SellComponent implements OnInit {
   //     this.addItem();
   // }
   tabChanged = (tabChangeEvent: MatTabChangeEvent): void => {
-    if(tabChangeEvent.index==0){
+    if (tabChangeEvent.index == 0) {
       // this.centralForm.get('country').setValue('');
       // this.centralForm.get('mins').setValue(0);
       // this.centralForm.get('cost_mins').setValue(0);
@@ -278,7 +285,7 @@ export class SellComponent implements OnInit {
       // this.panelOpenState = false;
 
     }
-    if(tabChangeEvent.index==1){
+    if (tabChangeEvent.index == 1) {
       // this.clientDebitForm.get('searchClient').setValue('');
       // this.clientDebitForm.get('clientID').setValue('');
       // this.clientDebitForm.get('clientName').setValue('');
@@ -288,7 +295,7 @@ export class SellComponent implements OnInit {
       // this.panelOpenState = false;
     }
   }
-  openClientModal(clientModal){
+  openClientModal(clientModal) {
     this.modalReference = this.modalService.open(clientModal, { centered: true, ariaLabelledBy: 'modal-basic-title' });
     var name = '';
     var phone = '';
@@ -308,16 +315,16 @@ export class SellComponent implements OnInit {
     this.modalReference.close();
     document.getElementById("searchClient").focus();
   }
-  clearForm(){
+  clearForm() {
     this.clientForm.resetForm();
   }
-  sellFullCard(){
+  sellFullCard() {
     const fullCardformValue = this.fullCardForm.value;
     const clientDebitformValue = this.clientDebitForm.value;
-    if(fullCardformValue['debit']==true && clientDebitformValue['clientID']!='')
-      fullCardformValue['clientID']=clientDebitformValue['clientID'];
+    if (fullCardformValue['debit'] == true && clientDebitformValue['clientID'] != '')
+      fullCardformValue['clientID'] = clientDebitformValue['clientID'];
     else
-      fullCardformValue['clientID']=1;
+      fullCardformValue['clientID'] = 1;
     this.sellService.sellFullCard(this.fullCardForm.value).subscribe(Response => {
     }, error => {
       alert(error)
@@ -334,13 +341,13 @@ export class SellComponent implements OnInit {
     this.clientDebitForm.get('clientName').setValue('');
     this.clientDebitForm.get('clientID').setValue('');
   }
-  sellOffers(){
+  sellOffers() {
     const offersformValue = this.offersForm.value;
     const clientDebitformValue = this.clientDebitForm.value;
-    if(offersformValue['debit']==true && clientDebitformValue['clientID']!='')
-      offersformValue['clientID']=clientDebitformValue['clientID'];
+    if (offersformValue['debit'] == true && clientDebitformValue['clientID'] != '')
+      offersformValue['clientID'] = clientDebitformValue['clientID'];
     else
-      offersformValue['clientID']=1;
+      offersformValue['clientID'] = 1;
     this.sellService.sellOffers(this.offersForm.value).subscribe(Response => {
     }, error => {
       alert(error)
@@ -357,13 +364,13 @@ export class SellComponent implements OnInit {
     this.clientDebitForm.get('clientName').setValue('');
     this.clientDebitForm.get('clientID').setValue('');
   }
-  sellCreditTransfers(){
+  sellCreditTransfers() {
     const creditTransfersFormValue = this.creditTransfersForm.value;
     const clientDebitformValue = this.clientDebitForm.value;
-    if(creditTransfersFormValue['debit']==true && clientDebitformValue['clientID']!='')
-      creditTransfersFormValue['clientID']=clientDebitformValue['clientID'];
+    if (creditTransfersFormValue['debit'] == true && clientDebitformValue['clientID'] != '')
+      creditTransfersFormValue['clientID'] = clientDebitformValue['clientID'];
     else
-      creditTransfersFormValue['clientID']=1;
+      creditTransfersFormValue['clientID'] = 1;
     this.sellService.sellCreditTransfers(this.creditTransfersForm.value).subscribe(Response => {
     }, error => {
       alert(error)
@@ -377,10 +384,10 @@ export class SellComponent implements OnInit {
     this.clientDebitForm.get('clientName').setValue('');
     this.clientDebitForm.get('clientID').setValue('');
   }
-  sellAccessories(){
+  sellAccessories() {
 
   }
-  sellCentral(){
+  sellCentral() {
     const formValue = this.centralForm.value;
     this.sellService.addSellCentral(this.centralForm.value).subscribe(Response => {
     }, error => {
@@ -391,43 +398,43 @@ export class SellComponent implements OnInit {
     this.centralForm.get('price').setValue('');
   }
   get searchBarCodeFullCard() {
-  return this.fullCardForm.get('searchBarCode');
-  } 
+    return this.fullCardForm.get('searchBarCode');
+  }
   get cardName() {
     return this.fullCardForm.get('cardName');
-  } 
+  }
   get quantityFullCard() {
     return this.fullCardForm.get('quantity');
-  }  
+  }
   get priceFullCard() {
     return this.fullCardForm.get('price');
-  } 
+  }
   get searchBarCodeDays() {
     return this.offersForm.get('searchBarCode');
-  } 
-  get companyDays() {
-      return this.offersForm.get('company');
   }
-   
+  get companyDays() {
+    return this.offersForm.get('company');
+  }
+
   get mounthDays() {
     return this.offersForm.get('mounth');
-  } 
+  }
   get creditsDays() {
     return this.offersForm.get('credits');
   }
   get priceDays() {
     return this.offersForm.get('price');
-  } 
-   
+  }
+
   get companyCreditTransfers() {
     return this.creditTransfersForm.get('company');
-  } 
+  }
   get creditscreditTransfers() {
     return this.creditTransfersForm.get('credits');
   }
   get pricecreditTransfers() {
     return this.creditTransfersForm.get('price');
-  }  
+  }
   get name() {
     return this.clientForm.get('name');
   }
@@ -449,7 +456,7 @@ export class SellComponent implements OnInit {
   get country() {
     return this.centralForm.get('country');
   }
-  
+
   get price() {
     return this.centralForm.get('price');
   }
